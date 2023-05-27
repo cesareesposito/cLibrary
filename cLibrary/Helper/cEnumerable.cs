9802e4b6eb67d96b83e721ae64e4742b0baad80f
cesareesposito <46751367+cesareesposito@users.noreply.github.com>
2023-05-27 17:47:31 +0200

    Enumerable Extensions
    add cForEach

---
+++ cLibrary/Helper/cEnumerable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cLibrary.Helper
+{
+    public static class cEnumerableExtensions
+    {
+        public static void cForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+                action(element);
+        }
+    }
+}
