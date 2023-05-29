912a7611ae0c7aa92cded5abdfd90bc8f7da32cb
Cesare Esposito <46751367+cesareesposito@users.noreply.github.com>
2023-05-29 17:32:59 +0200

    task example

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,69 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using cLibrary.Models.Base;
-using cLibrary.Models;
-using cLibrary.Helper;
-
-string numberString = "10";
-int n1 = numberString.ToInt();
-Console.WriteLine(n1);  // Output: 10
-
-string date = "2023-05-27";
-DateTime dateTime = date.ToDateTime();
-Console.WriteLine(dateTime);  // Output: 2023-05-27 00:00:00
-Console.ReadLine();
-
-class Dto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
-class Filter : cLibrary.Models.Base.Filter { }
-class Service
-{
-    private List<Dto> dtos = new()
-    {
-        new Dto { Id = 1, Name = "Mario" },
-        new Dto { Id = 2, Name = "Rossi" }
-    };
-    async Task<DataSource<Dto>> Get()
-    {
-        var filter = new Filter()
-        {
-            PageSize = 10,
-            SortField = "Id",
-            //cLibrary.models.Enums.SortDirection.Descending = 1
-            //cLibrary.models.Enums.SortDirection.Ascending = 0
-            //SortOrder = cLibrary.models.Enums.SortDirection.Descending,
-            //or
-            SortOrder = cLibrary.Models.Enums.SortDirection.Descending
-        };
-
-        dtos.cForEach(it => it.Name = "3333");
-
-        var result = dtos
-            .AsQueryable()
-            .ApplyBaseFilterAsync(filter);
-
-        return await result;
-        // Output: 
-        //"DataSource": {
-        //    "TotalItems": 2,
-        //    "Items": [
-        //      {
-        //        "Id": 2,
-        //        "Name": "Rossi"
-        //      },
-        //      {
-        //        "Id": 1,
-        //        "Name": "Mario"
-        //      }
-        //    ] 
-        //  }
-    }
-}
-
 
 
 
---
+++ Test/TaskExample.cs
@@ -0,0 +1,51 @@
+﻿using cLibrary.Models.Base;
+using cLibrary.Models.Enums;
+using cLibrary.Models.Task;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Param
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string tag { set; get; }
+    }
+    public class CaricaFile : TaskWorker
+    {
+        private string _param1;
+        private string _param2;
+        public CaricaFile(TaskDescriptor task, string param1,string param2)
+            : base(task)
+        {
+            _param1 = param1;
+            _param2 = param2;
+            _task = task;
+        }
+
+
+        public override void TaskWork()
+        {
+            AddLogElement(0, _param1, LogSeverity.Info, "messaggio");
+            var task = _task;
+            var del = _task.TaskDelegate;
+            Thread.Sleep(1000);
+
+            AddLogElement(34, null, LogSeverity.Info, task.Title);
+
+            var parm = (Param)_task.TaskParams;
+
+
+            _task.TaskResult = TaskResult.SUCCESS;
+        }
+
+    }
+    internal class TaskExample
+    {
+    }
+}
