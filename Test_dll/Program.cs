7783fe95679738cdf4f6ab93de5772ef30e61b5a
Cesare Esposito <46751367+cesareesposito@users.noreply.github.com>
2023-05-31 13:24:21 +0200

    test

--- Test_dll/Program.cs
+++ Test_dll/Program.cs
@@ -6,6 +6,63 @@ using cLibrary.Models;
 using Test_dll;
 using cLibrary.Helper;
 
+//List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+//List<Person> people = new List<Person>
+//{
+//    new Person { Name = "Alice", Age = 25 },
+//    new Person { Name = "Bob", Age = 30 },
+//    new Person { Name = "Charlie", Age = 35 }
+//};
+
+//var numResultp = people.cForEach(it =>
+//{
+//    return 100;
+//});
+
+//var sResultp = list.cForEach(it =>
+//{
+//    return "1";
+//});
+
+//var oResultp = people.cForEach(it =>
+//{
+//    return new OperationResult();
+//});
+
+//people.cForEach(it =>
+//{
+//    // Fai qualcosa
+//});
+
+//people.cForEach(it =>
+//{
+//    if (it.Name == "") return it;
+
+
+//    // Fai qualcosa
+//});
+
+//var numResult = list.cForEach(it =>
+//{
+//    return 100;
+//});
+
+//var sResult = list.cForEach(it =>
+//{
+//    return "1";
+//});
+
+//var oResult = list.cForEach(it =>
+//{
+//    return new OperationResult();
+//});
+
+//list.cForEach(it =>
+//{
+//    // Fai qualcosa
+//});
+
+
 List<int> list = new List<int> { 1, 2, 3, 4, 5 };
 List<Person> people = new List<Person>
 {
@@ -14,54 +71,19 @@ List<Person> people = new List<Person>
     new Person { Name = "Charlie", Age = 35 }
 };
 
-var numResultp = people.cForEach(it =>
-{
-    return 100;
-});
-
-var sResultp = list.cForEach(it =>
-{
-    return "1";
-});
-
-var oResultp = people.cForEach(it =>
-{
-    return new OperationResult();
-});
-
 people.cForEach(it =>
 {
     // Fai qualcosa
 });
 
-people.cForEach(it =>
+var foundResult = list.cForEach(it =>
 {
-    if (it.Name == "") return it;
-
-
-    return null;
-    // Fai qualcosa
+    if (it == 3)
+        return "trovato";
+    // Non restituire nulla in caso contrario
 });
 
-var numResult = list.cForEach(it =>
-{
-    return 100;
-});
-
-var sResult = list.cForEach(it =>
-{
-    return "1";
-});
-
-var oResult = list.cForEach(it =>
-{
-    return new OperationResult();
-});
-
-list.cForEach(it =>
-{
-    // Fai qualcosa
-});
+// Utilizza foundResult come desiderato
 
 
 Console.WriteLine("Hello, World!");
--- cLibrary/Helper/cEnumerable.cs
+++ cLibrary/Helper/cEnumerable.cs
@@ -4,24 +4,53 @@ using cLibrary.Models.Base;
 namespace cLibrary.Helper
 {
     public static class cEnumerableExtensions
-    { 
-        public static TResult cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
-        {
-            foreach (var element in source)
-            {
-                var result = action(element);
-                if (result != null)
-                {
-                    return result;
-                }
-            }
-            return default;
-        }
-       
+    {
+        //public static TResult cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
+        //{
+        //    foreach (var element in source)
+        //    {
+        //        var result = action(element);
+        //        if (result != null)
+        //        {
+        //             return result;
+        //        }
+        //    }
+        //    return default;
+        //}
+
         public static void cForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
             foreach (T element in source)
+            {
                 action(element);
-        }        
+            }
+        }
+
+        public static TResult cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
+        {
+            bool returnResult = false;
+            TResult result = default(TResult);
+
+            foreach (var element in source)
+            {
+                TResult currentResult = action(element);
+
+                if (!EqualityComparer<TResult>.Default.Equals(currentResult, default(TResult)))
+                {
+                    returnResult = true;
+                    result = currentResult;
+                    break;
+                }
+            }
+
+            if (returnResult)
+            {
+                return result;
+            }
+
+            return default(TResult);
+        }
+
+
     }
 }
