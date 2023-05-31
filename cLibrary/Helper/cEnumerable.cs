f08f90c08bbac9b488e5ed5ebede52bd481d841a
Cesare Esposito <46751367+cesareesposito@users.noreply.github.com>
2023-05-31 10:09:52 +0200

    Improve cForEach

--- cLibrary/Helper/cEnumerable.cs
+++ cLibrary/Helper/cEnumerable.cs
@@ -2,10 +2,14 @@
 {
     public static class cEnumerableExtensions
     {
-        public static void cForEach<T>(this IEnumerable<T> source, Action<T> action)
+        public static IEnumerable<T> cForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
             foreach (T element in source)
+            {
                 action(element);
+                yield return element;
+            }
         }
+
     }
 }
