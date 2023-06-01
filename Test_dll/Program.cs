04a4b46c4f41641ae700511fefc56e87897cfb53
Cesare Esposito <46751367+cesareesposito@users.noreply.github.com>
2023-06-01 08:44:05 +0200

    update cForEach

--- Test_dll/Program.cs
+++ Test_dll/Program.cs
@@ -1,67 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-using cLibrary.Models.Base;
-using cLibrary.Models;
 using Test_dll;
 using cLibrary.Helper;
-
-//List<int> list = new List<int> { 1, 2, 3, 4, 5 };
-//List<Person> people = new List<Person>
-//{
-//    new Person { Name = "Alice", Age = 25 },
-//    new Person { Name = "Bob", Age = 30 },
-//    new Person { Name = "Charlie", Age = 35 }
-//};
-
-//var numResultp = people.cForEach(it =>
-//{
-//    return 100;
-//});
-
-//var sResultp = list.cForEach(it =>
-//{
-//    return "1";
-//});
-
-//var oResultp = people.cForEach(it =>
-//{
-//    return new OperationResult();
-//});
-
-//people.cForEach(it =>
-//{
-//    // Fai qualcosa
-//});
-
-//people.cForEach(it =>
-//{
-//    if (it.Name == "") return it;
-
-
-//    // Fai qualcosa
-//});
-
-//var numResult = list.cForEach(it =>
-//{
-//    return 100;
-//});
-
-//var sResult = list.cForEach(it =>
-//{
-//    return "1";
-//});
-
-//var oResult = list.cForEach(it =>
-//{
-//    return new OperationResult();
-//});
-
-//list.cForEach(it =>
-//{
-//    // Fai qualcosa
-//});
-
+using cLibrary.Models;
 
 List<int> list = new List<int> { 1, 2, 3, 4, 5 };
 List<Person> people = new List<Person>
@@ -71,19 +12,60 @@ List<Person> people = new List<Person>
     new Person { Name = "Charlie", Age = 35 }
 };
 
+var numResultp = people.cForEach(it =>
+{
+    return 100;
+});
+
+Console.WriteLine(numResultp);
+
+var sResultp = list.cForEach(it =>
+{
+    return "1";
+});
+Console.WriteLine(sResultp);
+
+var oResultp = people.cForEach(it =>
+{
+    return new OperationResult();
+});
+
+Console.WriteLine(oResultp.);
+
 people.cForEach(it =>
 {
     // Fai qualcosa
 });
 
-list.cForEach(it =>
+var res = people.cForEach(it =>
 {
-    if (it == 3)
-        return "trovato";
-    // Non restituire nulla in caso contrario
+    if (it.Name == "") return it;
+
+    return null;
+    // Fai qualcosa
 });
 
-// Utilizza foundResult come desiderato
+Console.WriteLine(res.Name);
+
+var numResult = list.cForEach(it =>
+{
+    return 100;
+});
+
+var sResult = list.cForEach(it =>
+{
+    return "1";
+});
+
+var oResult = list.cForEach(it =>
+{
+    return new OperationResult();
+});
+
+list.cForEach(it =>
+{
+    // Fai qualcosa
+});
 
 
 Console.WriteLine("Hello, World!");
--- cLibrary/Helper/cEnumerable.cs
+++ cLibrary/Helper/cEnumerable.cs
@@ -5,51 +5,47 @@ namespace cLibrary.Helper
 {
     public static class cEnumerableExtensions
     {
-        //public static TResult cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
-        //{
-        //    foreach (var element in source)
-        //    {
-        //        var result = action(element);
-        //        if (result != null)
-        //        {
-        //             return result;
-        //        }
-        //    }
-        //    return default;
-        //}
+        public static TResult? cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
+        {
+            foreach (var element in source)
+            {
+                var result = action(element);
+                if (result != null)
+                    return result;
+            }
+            return default;
+        }
 
         public static void cForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
             foreach (T element in source)
-            {
                 action(element);
-            }
         }
 
-        public static TResult cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
-        {
-            bool returnResult = false;
-            TResult result = default(TResult);
+        //public static TResult cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
+        //{
+        //    bool returnResult = false;
+        //    TResult result = default(TResult);
 
-            foreach (var element in source)
-            {
-                TResult currentResult = action(element);
+        //    foreach (var element in source)
+        //    {
+        //        TResult currentResult = action(element);
 
-                if (!EqualityComparer<TResult>.Default.Equals(currentResult, default(TResult)))
-                {
-                    returnResult = true;
-                    result = currentResult;
-                    break;
-                }
-            }
+        //        if (!EqualityComparer<TResult>.Default.Equals(currentResult, default(TResult)))
+        //        {
+        //            returnResult = true;
+        //            result = currentResult;
+        //            break;
+        //        }
+        //    }
 
-            if (returnResult)
-            {
-                return result;
-            }
+        //    if (returnResult)
+        //    {
+        //        return result;
+        //    }
 
-            return default(TResult);
-        }
+        //    return default(TResult);
+        //}
 
 
     }
