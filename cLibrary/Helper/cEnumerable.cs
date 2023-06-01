7ba7279cd591f44017db13a4d6ea9746945ec0be
Cesare Esposito <46751367+cesareesposito@users.noreply.github.com>
2023-06-01 10:34:41 +0200

    Update cEnumerable.cs
    cForEachAsync

--- cLibrary/Helper/cEnumerable.cs
+++ cLibrary/Helper/cEnumerable.cs
@@ -15,6 +15,19 @@ namespace cLibrary.Helper
             }
             return default;
         }
+        public static async Task<TResult?> cForEachAsync<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
+        {
+            foreach (var element in source)
+            {
+                var result = action(element);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            return default;
+        }
+
 
         public static void cForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
