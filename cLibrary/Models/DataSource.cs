a5cf78e25d23ef0783c720f2c0f633069d81863c
cesareesposito <46751367+cesareesposito@users.noreply.github.com>
2023-06-03 19:00:48 +0200

    update
    add DataSource costructor

--- cLibrary/Models/DataSource.cs
+++ cLibrary/Models/DataSource.cs
@@ -2,6 +2,10 @@
 {
     public class DataSource<T>
     {
+        public DataSource()
+        {
+            Items = new List<T>();
+        }
         public int TotalItems { get; set; }
         public IEnumerable<T> Items { get; set; }
     }
