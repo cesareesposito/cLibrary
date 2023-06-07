0ad4176bc8402f4103b8b4ec19b70b6b3d59ba25
cesareesposito <46751367+cesareesposito@users.noreply.github.com>
2023-06-07 20:59:23 +0200

    Update OperationResult.cs

--- cLibrary/Models/OperationResult.cs
+++ cLibrary/Models/OperationResult.cs
@@ -8,8 +8,8 @@ namespace cLibrary.Models
     [Serializable]
     public class OperationResult
     {
-        private const string DefaultErrorMessage = "Si è verificato un errore imprevisto nell'elaborazione della richiesta. Contattare il supporto tecnico per maggiori informazioni.";
-        private const string DefaultWarningMessage = "L'operazione non ha prodotto nessuna modifica. Contattare il supporto tecnico per maggiori informazioni.";
+        public const string DefaultErrorMessage = "Si è verificato un errore imprevisto nell'elaborazione della richiesta. Contattare il supporto tecnico per maggiori informazioni.";
+        public const string DefaultWarningMessage = "L'operazione non ha prodotto nessuna modifica. Contattare il supporto tecnico per maggiori informazioni.";
         public const string DefaultMessage = "Operazione completata.";
 
         #region altri costruttori    
