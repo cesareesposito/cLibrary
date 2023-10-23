aad7f4ee2d37ffaae27e1bff055d5e2e631a0fbb
cesareesposito <46751367+cesareesposito@users.noreply.github.com>
2023-10-23 13:57:37 +0200

    update

--- cLibrary/Helper/cEnumerable.cs
+++ cLibrary/Helper/cEnumerable.cs
@@ -4,6 +4,8 @@
     {
         public static TResult? cForEach<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
         {
+            if (source == null) return default;
+
             foreach (var element in source)
             {
                 var result = action(element);
@@ -14,6 +16,8 @@
         }
         public static async Task<TResult?> cForEachAsync<T, TResult>(this IEnumerable<T> source, Func<T, TResult> action)
         {
+            if (source == null) return default;
+
             foreach (var element in source)
             {
                 var result = action(element);
@@ -28,12 +32,16 @@
 
         public static void cForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
+            if (source == null) return;
+
             foreach (T element in source)
                 action(element);
         }
        
         public static async Task cForEachAsync<T>(this IEnumerable<T> source, Action<T> action)
         {
+            if (source == null) return;
+
             await Task.WhenAll(source.Select(element => Task.Run(() => action(element))));
         }
     }
