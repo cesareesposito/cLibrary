dbfe201669a695ce6a7452ab5cf852f4ed79a536
cesareesposito <46751367+cesareesposito@users.noreply.github.com>
2023-11-03 00:39:36 +0100

    update

--- cLibrary.Helper/jwe.cs
+++ cLibrary.Helper/jwe.cs
@@ -1,74 +1,74 @@
-﻿using Microsoft.IdentityModel.JsonWebTokens;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Microsoft.IdentityModel.JsonWebTokens;
+//using Microsoft.IdentityModel.Tokens;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Cryptography;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp1
-{
-    public class Jwe
-    {
-        public void CreateAndValidateJwe()
-        {
-            var encryptionKey = RSA.Create(3072); // public key for encryption, private key for decryption
-            var signingKey = ECDsa.Create(ECCurve.NamedCurves.nistP256); // private key for signing, public key for validation
+//namespace ConsoleApp1
+//{
+//    public class Jwe
+//    {
+//        public void CreateAndValidateJwe()
+//        {
+//            var encryptionKey = RSA.Create(3072); // public key for encryption, private key for decryption
+//            var signingKey = ECDsa.Create(ECCurve.NamedCurves.nistP256); // private key for signing, public key for validation
 
-            var encryptionKid = Guid.NewGuid().ToString("N");
-            var signingKid = Guid.NewGuid().ToString("N");
+//            var encryptionKid = Guid.NewGuid().ToString("N");
+//            var signingKid = Guid.NewGuid().ToString("N");
 
-            var privateEncryptionKey = new RsaSecurityKey(encryptionKey) { KeyId = encryptionKid };
-            var publicEncryptionKey = new RsaSecurityKey(encryptionKey.ExportParameters(false)) { KeyId = encryptionKid };
-            var privateSigningKey = new ECDsaSecurityKey(signingKey) { KeyId = signingKid };
-            var publicSigningKey = new ECDsaSecurityKey(ECDsa.Create(signingKey.ExportParameters(false))) { KeyId = signingKid };
+//            var privateEncryptionKey = new RsaSecurityKey(encryptionKey) { KeyId = encryptionKid };
+//            var publicEncryptionKey = new RsaSecurityKey(encryptionKey.ExportParameters(false)) { KeyId = encryptionKid };
+//            var privateSigningKey = new ECDsaSecurityKey(signingKey) { KeyId = signingKid };
+//            var publicSigningKey = new ECDsaSecurityKey(ECDsa.Create(signingKey.ExportParameters(false))) { KeyId = signingKid };
 
-            var token = CreateJwe(privateSigningKey, publicEncryptionKey);
-            Console.WriteLine("Token JWT con JWE generato:");
-            Console.WriteLine(token);
-            var result = DecryptAndValidateJwe(token, publicSigningKey, privateEncryptionKey);
-        }
+//            var token = CreateJwe(privateSigningKey, publicEncryptionKey);
+//            Console.WriteLine("Token JWT con JWE generato:");
+//            Console.WriteLine(token);
+//            var result = DecryptAndValidateJwe(token, publicSigningKey, privateEncryptionKey);
+//        }
 
-        private static string CreateJwe(SecurityKey signingKey, SecurityKey encryptionKey)
-        {
-            var handler = new JsonWebTokenHandler();
+//        private static string CreateJwe(SecurityKey signingKey, SecurityKey encryptionKey)
+//        {
+//            var handler = new JsonWebTokenHandler();
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Audience = "api1",
-                Issuer = "https://idp.example.com",
-                Claims = new Dictionary<string, object> { { "sub", "811e790749a24d8a8f766e1a44dca28a" } },
+//            var tokenDescriptor = new SecurityTokenDescriptor
+//            {
+//                Audience = "api1",
+//                Issuer = "https://idp.example.com",
+//                Claims = new Dictionary<string, object> { { "sub", "811e790749a24d8a8f766e1a44dca28a" } },
 
-                // private key for signing
-                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.EcdsaSha256),
+//                // private key for signing
+//                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.EcdsaSha256),
 
-                // public key for encryption
-                EncryptingCredentials = new EncryptingCredentials(encryptionKey, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes256CbcHmacSha512)
-            };
+//                // public key for encryption
+//                EncryptingCredentials = new EncryptingCredentials(encryptionKey, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes256CbcHmacSha512)
+//            };
 
-            return handler.CreateToken(tokenDescriptor);
-        }
+//            return handler.CreateToken(tokenDescriptor);
+//        }
 
-        private static bool DecryptAndValidateJwe(string token, SecurityKey signingKey, SecurityKey encryptionKey)
-        {
-            var handler = new JsonWebTokenHandler();
+//        private static bool DecryptAndValidateJwe(string token, SecurityKey signingKey, SecurityKey encryptionKey)
+//        {
+//            var handler = new JsonWebTokenHandler();
 
-            TokenValidationResult result = handler.ValidateToken(
-                token,
-                new TokenValidationParameters
-                {
-                    ValidAudience = "api1",
-                    ValidIssuer = "https://idp.example.com",
+//            TokenValidationResult result = handler.ValidateToken(
+//                token,
+//                new TokenValidationParameters
+//                {
+//                    ValidAudience = "api1",
+//                    ValidIssuer = "https://idp.example.com",
 
-                    // public key for signing
-                    IssuerSigningKey = signingKey,
+//                    // public key for signing
+//                    IssuerSigningKey = signingKey,
 
-                    // private key for encryption
-                    TokenDecryptionKey = encryptionKey
-                });
+//                    // private key for encryption
+//                    TokenDecryptionKey = encryptionKey
+//                });
 
-            return result.IsValid;
-        }
-    }
-}
+//            return result.IsValid;
+//        }
+//    }
+//}
